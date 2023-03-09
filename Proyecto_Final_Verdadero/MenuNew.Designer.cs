0d726eee28f2c506b2229b596d09ad4cc775dea9
Andy Bonilla <Andy Bonilla@DESKTOP-OG2M4P6>
2023-03-08 19:38:42 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto_Final_Verdadero/MenuNew.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Proyecto_Final_Verdadero
+{
+    partial class MenuNew
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MenuNew));
+            this.pnlSuperior = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pcbMinimize = new System.Windows.Forms.PictureBox();
+            this.pcbClose = new System.Windows.Forms.PictureBox();
+            this.pnlMenu = new System.Windows.Forms.Panel();
+            this.btnCreditos = new System.Windows.Forms.Button();
+            this.pnlPrincipal = new System.Windows.Forms.Panel();
+            this.btnMenu = new System.Windows.Forms.Button();
+            this.pnlSuperior.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbMinimize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbClose)).BeginInit();
+            this.pnlMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlSuperior
+            // 
+            this.pnlSuperior.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(3)))), ((int)(((byte)(9)))));
+            this.pnlSuperior.Controls.Add(this.label1);
+            this.pnlSuperior.Controls.Add(this.pcbMinimize);
+            this.pnlSuperior.Controls.Add(this.pcbClose);
+            this.pnlSuperior.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlSuperior.Location = new System.Drawing.Point(0, 0);
+            this.pnlSuperior.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pnlSuperior.Name = "pnlSuperior";
+            this.pnlSuperior.Size = new System.Drawing.Size(1201, 33);
+            this.pnlSuperior.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(4, 1);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(239, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Proyecto Final V7.0";
+            // 
+            // pcbMinimize
+            // 
+            this.pcbMinimize.Image = ((System.Drawing.Image)(resources.GetObject("pcbMinimize.Image")));
+            this.pcbMinimize.Location = new System.Drawing.Point(1111, 0);
+            this.pcbMinimize.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pcbMinimize.Name = "pcbMinimize";
+            this.pcbMinimize.Size = new System.Drawing.Size(33, 33);
+            this.pcbMinimize.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcbMinimize.TabIndex = 3;
+            this.pcbMinimize.TabStop = false;
+            this.pcbMinimize.Click += new System.EventHandler(this.pcbMinimize_Click);
+            // 
+            // pcbClose
+            // 
+            this.pcbClose.Image = ((System.Drawing.Image)(resources.GetObject("pcbClose.Image")));
+            this.pcbClose.Location = new System.Drawing.Point(1152, 0);
+            this.pcbClose.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pcbClose.Name = "pcbClose";
+            this.pcbClose.Size = new System.Drawing.Size(33, 33);
+            this.pcbClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcbClose.TabIndex = 2;
+            this.pcbClose.TabStop = false;
+            this.pcbClose.Click += new System.EventHandler(this.pcbClose_Click);
+            // 
+            // pnlMenu
+            // 
+            this.pnlMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(3)))), ((int)(((byte)(9)))));
+            this.pnlMenu.Controls.Add(this.btnCreditos);
+            this.pnlMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlMenu.Location = new System.Drawing.Point(0, 33);
+            this.pnlMenu.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pnlMenu.Name = "pnlMenu";
+            this.pnlMenu.Size = new System.Drawing.Size(48, 644);
+            this.pnlMenu.TabIndex = 1;
+            // 
+            // btnCreditos
+            // 
+            this.btnCreditos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(13)))), ((int)(((byte)(22)))));
+            this.btnCreditos.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCreditos.Font = new System.Drawing.Font("MS Reference Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreditos.ForeColor = System.Drawing.SystemColors.Info;
+            this.btnCreditos.Image = ((System.Drawing.Image)(resources.GetObject("btnCreditos.Image")));
+            this.btnCreditos.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCreditos.Location = new System.Drawing.Point(9, 587);
+            this.btnCreditos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCreditos.Name = "btnCreditos";
+            this.btnCreditos.Size = new System.Drawing.Size(275, 44);
+            this.btnCreditos.TabIndex = 0;
+            this.btnCreditos.Text = "Creditos";
+            this.btnCreditos.UseVisualStyleBackColor = false;
+            this.btnCreditos.Click += new System.EventHandler(this.btnCreditos_Click);
+            // 
+            // pnlPrincipal
+            // 
+            this.pnlPrincipal.Location = new System.Drawing.Point(56, 54);
+            this.pnlPrincipal.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pnlPrincipal.Name = "pnlPrincipal";
+            this.pnlPrincipal.Size = new System.Drawing.Size(1132, 623);
+            this.pnlPrincipal.TabIndex = 2;
+            // 
+            // btnMenu
+            // 
+            this.btnMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(13)))), ((int)(((byte)(22)))));
+            this.btnMenu.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnMenu.Font = new System.Drawing.Font("MS Reference Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMenu.ForeColor = System.Drawing.SystemColors.Info;
+            this.btnMenu.Image = ((System.Drawing.Image)(resources.GetObject("btnMenu.Image")));
+            this.btnMenu.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMenu.Location = new System.Drawing.Point(56, 41);
+            this.btnMenu.Margin = new System.Windows.Forms.Padding(4);
+            this.btnMenu.Name = "btnMenu";
+            this.btnMenu.Size = new System.Drawing.Size(127, 45);
+            this.btnMenu.TabIndex = 1;
+            this.btnMenu.Text = "Menú";
+            this.btnMenu.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnMenu.UseVisualStyleBackColor = false;
+            this.btnMenu.Click += new System.EventHandler(this.btnMenu_Click);
+            // 
+            // MenuNew
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(19)))), ((int)(((byte)(32)))));
+            this.ClientSize = new System.Drawing.Size(1201, 677);
+            this.Controls.Add(this.btnMenu);
+            this.Controls.Add(this.pnlMenu);
+            this.Controls.Add(this.pnlSuperior);
+            this.Controls.Add(this.pnlPrincipal);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "MenuNew";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MenuNew";
+            this.Load += new System.EventHandler(this.MenuNew_Load);
+            this.pnlSuperior.ResumeLayout(false);
+            this.pnlSuperior.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbMinimize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbClose)).EndInit();
+            this.pnlMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlSuperior;
+        private System.Windows.Forms.Panel pnlMenu;
+        private System.Windows.Forms.Button btnCreditos;
+        private System.Windows.Forms.PictureBox pcbMinimize;
+        private System.Windows.Forms.PictureBox pcbClose;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel pnlPrincipal;
+        private System.Windows.Forms.Button btnMenu;
+    }
+}
---
+++ Proyecto_Final_Verdadero/MenuNew.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Proyecto_Final_Verdadero
+{
+    public partial class MenuNew : Form
+    {
+        public MenuNew()
+        {
+            InitializeComponent();
+        }
+
+        #region Eventos
+        //Click en el boton para cerrar formulario
+        private void pcbClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //Click en el boton para minimizar el formulario
+        private void pcbMinimize_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+        #endregion
+
+        private void btnCreditos_Click(object sender, EventArgs e)
+        {
+            #region muestra formulario
+            pnlPrincipal.Controls.Clear();//Limpiamos panel
+            Form Pantalla = null;//Definimos variable pantalla
+
+            Pantalla = new Forms.Creditos();//Pantalla es igual al form Creditos
+            Pantalla.TopLevel = false;//Definimos como pantalla secundaria
+            pnlPrincipal.Controls.Add(Pantalla);//Añadimos la pantalla al panel
+            Pantalla.Show();//Mostramos la pantalla
+            #endregion
+            dplMenu();
+        }
+
+        private void btnMenu_Click(object sender, EventArgs e)
+        {
+            if (pnlMenu.Width == 48)
+            {
+                dplMenu(false);
+            }
+            else 
+            {
+                dplMenu();
+            }            
+        }
+
+        private void MenuNew_Load(object sender, EventArgs e)
+        {
+            dplMenu();
+        }
+
+        private void dplMenu(bool ini = true)
+        {
+            if (ini)
+            {
+                btnMenu.Location = new Point(56, 41);
+                pnlMenu.Width = 48;
+            }
+            else
+            {
+                btnMenu.Location = new Point(308, 41);
+                pnlMenu.Width = 300;
+            }
+        }
+    }
+}
